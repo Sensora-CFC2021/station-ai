5191182c920b727d298d388b890f1ac4b7d1ccea
Itgel917 <75331283+Itgel917@users.noreply.github.com>
2021-07-30 13:44:06 +0800

    Update SpeechAdvice.py

--- textToSpeech/SpeechAdvice.py
+++ textToSpeech/SpeechAdvice.py
@@ -1,5 +1,5 @@
 def speech_advice(language, soil_moisture_percentage, temperature, water_advice):
     result = "Soil moisture is " + str(soil_moisture_percentage) + "percent<break time='1s'/>" + \
-             "Temperature is " + " 27 celsius<break time='1s'/>" + \
+             "Temperature is " + str(temperature) + "celsius<break time='1s'/>" + \
              "Watering advice " + str(water_advice.watering_decision) + " "
     return result
